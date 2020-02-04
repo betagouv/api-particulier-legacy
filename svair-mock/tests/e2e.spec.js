1f02a7cf7726f49a215d0ef2096c1f6071f04d13
Stanislas Bernard <bernardstanislas@gmail.com>
2020-02-04 18:15:48 +0100

    :white_check_mark: Snapshot mock result

---
+++ svair-mock/tests/e2e.spec.js
@@ -0,0 +1,25 @@
+const request = require('supertest');
+const app = require('../app');
+
+describe('The mock application', () => {
+    let server;
+
+    it('renders a mocked html page when called with test inputs', () => {
+        server = request(app)
+            .post('/secavis/faces/commun/index.jsf')
+            .send('j_id_7:j_id_l=Valider')
+            .send('j_id_7_SUBMIT=1')
+            .send('j_id_7:spi=1906299999002')
+            .send('j_id_7:num_facture=1906299999002')
+            .expect(200)
+            .then(response => {
+                expect(response.text).toMatchSnapshot();
+            })
+
+        return server;
+    });
+
+    afterAll(() => {
+        server.stop();
+    })
+});
