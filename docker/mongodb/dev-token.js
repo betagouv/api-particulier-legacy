bb2d73f7224d0c87204950dabe1aaa67367666dd
Stanislas Bernard <bernardstanislas@gmail.com>
2020-02-13 12:01:01 +0100

    :sparkles: Load dev token on mongo container creation

---
+++ docker/mongodb/dev-token.js
@@ -0,0 +1,8 @@
+db.tokens.insertOne({
+    name: "MAIRIE DE PARIS - 100",
+    email: "mairie-de-paris@yopmail.com",
+    scopes: [ "dgfip_avis_imposition", "cnaf_attestation_droits", "mesri_statut_etudiant" ],
+    signup_id: "100",
+    hashed_token: "5f6c070b2f2126b39cb8c72cd05e99588c189a7ef689d7f7c5a0d1ed337068e525485469aa007f7202335774150474875a9ac28d5b6f433d103dcd4e25fc48e4",
+    api_key_issued_at: "2020-02-10T13:19:31Z"
+});
