77a2cd7487066016297749a665111d232b47db62
Stanislas Bernard <bernardstanislas@gmail.com>
2020-02-14 12:07:17 +0100

    :sparkles: Working authentication

--- api-particulier/api/student/index.js
+++ api-particulier/api/student/index.js
@@ -1,18 +1,17 @@
 const express = require('express')
-const Auth = require('../../auth/auth')
+const authenticationMiddleware = require('../../auth/middleware')
 const Controller = require('./student.controller')
 const format = require('../lib/utils/format')
 const scopeAuthorization = require('../lib/middlewares/scopeAuthorization')
 
 module.exports = function (options) {
   const router = express.Router()
-  const auth = new Auth(options)
   const studentController = new Controller(options)
 
   router.get('/ping', studentController.ping, format)
   router.get(
     '/',
-    auth.canAccessApi,
+    authenticationMiddleware,
     studentController.student,
     studentController.authorize,
     scopeAuthorization,
