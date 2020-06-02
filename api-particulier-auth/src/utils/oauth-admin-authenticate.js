e1774eb534d82c26010e11a46594b717f116f5b5
Stanislas Bernard <bernardstanislas@gmail.com>
2020-06-02 17:21:01 +0200

    :lock: Use standard oauth library to authenticate users

--- api-particulier-auth/src/utils/oauth-admin-authenticate.js
+++ api-particulier-auth/src/utils/oauth-admin-authenticate.js
@@ -1,8 +1,7 @@
 import express from 'express';
 import sessions from 'client-sessions';
 import { isEmpty } from 'lodash';
-import oauth2 from 'simple-oauth2';
-import axios from 'axios';
+import { Issuer, generators } from 'openid-client'
 
 /*
  * This function returns a router that take care of the authentication process via oauth2.
@@ -10,7 +9,6 @@ import axios from 'axios';
  */
 const authenticate = function({
   oauthHost,
-  oauthUserInfoURL,
   clientID,
   clientSecret,
   host,
@@ -18,84 +16,78 @@ const authenticate = function({
   sessionSecret,
 }) {
   const oauthCallbackPath = '/oauth-callback';
-
-  // Configure Oauth2 authentication
-  const credentials = {
-    client: {
-      id: clientID,
-      secret: clientSecret,
-    },
-    auth: { tokenHost: oauthHost },
-  };
-
-  const oauth2Client = oauth2.create(credentials);
-
   // Creating a router to add authentication callback on it
   const authRouter = express.Router();
 
-  // Configuring session cookie middleware
-  authRouter.use(
-    sessions({
-      cookieName: 'session', // cookie name dictates the key name added to the request object
-      secret: sessionSecret, // should be a large unguessable string
-      duration: 2 * 60 * 60 * 1000, // how long the session will stay valid in ms
-      cookie: {
-        path: mountPointPath, // cookie will only be sent to requests under '/api'
-        httpOnly: true, // when true, cookie is not accessible from javascript
-        secureProxy: process.env.SECURE_PROXY === 'true',
-      },
-    })
-  );
+  Issuer.discover(`${oauthHost}/.well-known/openid-configuration`)
+    .then(authApiIssuer => {
+      const authApiClient = new authApiIssuer.Client({
+        client_id: clientID,
+        client_secret: clientSecret,
+        redirect_uris: [`${host}${mountPointPath}${oauthCallbackPath}`],
+        response_types: ['code'],
+        token_endpoint_auth_method: 'client_secret_post'
+      })
+      // Configuring session cookie middleware
+      authRouter.use(
+        sessions({
+          cookieName: 'session', // cookie name dictates the key name added to the request object
+          secret: sessionSecret, // should be a large unguessable string
+          duration: 2 * 60 * 60 * 1000, // how long the session will stay valid in ms
+          cookie: {
+            path: mountPointPath, // cookie will only be sent to requests under '/api'
+            httpOnly: true, // when true, cookie is not accessible from javascript
+            secureProxy: process.env.SECURE_PROXY === 'true',
+          },
+        })
+      );
 
-  authRouter.use((req, res, next) => {
-    if (req.path === oauthCallbackPath) {
-      return next();
-    }
+      authRouter.use((req, res, next) => {
+        if (req.path === oauthCallbackPath) {
+          return next();
+        }
 
-    if (isEmpty(req.session)) {
-      const authorizationUri = oauth2Client.authorizationCode.authorizeURL({
-        redirect_uri: `${host}${mountPointPath}${oauthCallbackPath}`,
-        scope: 'openid email roles',
-        state: `${req.originalUrl || '/admin/'}`,
+        if (isEmpty(req.session)) {
+          const nonce = generators.nonce();
+          const state = generators.state();
+          const authorizationUri = authApiClient.authorizationUrl({
+            redirect_uri: `${host}${mountPointPath}${oauthCallbackPath}`,
+            scope: 'openid email roles',
+            state,
+            nonce
+          });
+
+          req.session.nonce = nonce;
+          req.session.state = state;
+          req.session.originalUrl = req.originalUrl;
+
+          return res.redirect(authorizationUri);
+        }
+
+        next();
       });
 
-      return res.redirect(authorizationUri);
-    }
+      authRouter.get(oauthCallbackPath, async (req, res, next) => {
+        try {
+          const params = authApiClient.callbackParams(req);
+          const tokenSet = await authApiClient.callback(`${host}${mountPointPath}${oauthCallbackPath}`, params, {
+            state: req.session.state,
+            nonce: req.session.nonce
+          })
+          delete req.session.nonce;
+          delete req.session.state;
+          const { originalUrl } = req.session;
 
-    next();
-  });
+          const userInfo = await authApiClient.userinfo(tokenSet.access_token);
+          req.session = userInfo;
 
-  authRouter.get(oauthCallbackPath, async (req, res, next) => {
-    try {
-      if (req.query.error) {
-        throw new Error(`Oauth server says: ${req.query.error}, ${req.query.error_description}`)
-      }
-
-      const originalUrl = req.query.state;
-
-      const tokenConfig = {
-        code: req.query.code,
-        redirect_uri: `${host}${mountPointPath}${oauthCallbackPath}`,
-        scope: [],
-      };
-
-      const result = await oauth2Client.authorizationCode.getToken(tokenConfig);
-
-      const { data: userInfo } = await axios({
-        method: 'GET',
-        headers: { Authorization: `Bearer ${result.access_token}` },
-        url: oauthUserInfoURL,
+          res.redirect(originalUrl);
+        } catch (error) {
+          console.error(error);
+          res.sendStatus(401);
+        }
       });
-
-      req.session = userInfo;
-
-      res.redirect(originalUrl);
-    } catch (error) {
-      console.error(error);
-      res.sendStatus(401);
-    }
-  });
-
+    });
   return authRouter;
 };
 
